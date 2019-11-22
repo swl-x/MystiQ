fb90a6b8a6ff8737085183a35391007d37ff7b8d
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-11-21 22:36:51 -0500

    Refs #14, travis releases and less verbose testing

--- complete_appdir.sh
+++ complete_appdir.sh
@@ -1,18 +1,17 @@
 #!/bin/bash 
 
-# ffmpeg bins
-cp -v /usr/bin/ffmpeg AppDir/usr/bin
-
-# sox
-cp -v /usr/bin/sox AppDir/usr/bin
-mkdir -pv AppDir/usr/lib/mime/packages
-cp -v /usr/lib/mime/packages/sox AppDir/usr/lib/mime/packages/sox
+# pass the tools to the image
+TOOLS="ffmpeg ffplay ffprobe mplayer sox"
+for s in `echo "$TOOLS" | xargs` ; do
+    echo "Installing media tools: $s"
+    cp -v /usr/bin/$s AppDir/usr/bin
+done
 
 # copy all the icons
 mkdir -p AppDir/usr/share/icons/hicolor
 RES=`cd icons && ls | grep mystiq_| cut -d "_" -f 2 | cut -d "." -f 1`
 for r in `echo $RES | xargs` ; do
     target="AppDir/usr/share/icons/hicolor/$r/apps"
-    mkdir -pv "$target"
+    mkdir -p "$target"
     cp -v icons/mystiq_$r.png "$target/mystiq.png"
 done
