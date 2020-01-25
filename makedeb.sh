990620c196d29b5eca1094cf5aa18d096e43e72e
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2020-01-25 00:35:52 -0500

    Refs #37, add the template vars as well as the substitution procedure

--- makedeb.sh
+++ makedeb.sh
@@ -12,6 +12,14 @@ SRCDIR="$TMPDIR/$NAME-$VERSION"
 MAINT="Pavel Milanes Costa"
 EMAIL="pavelmc@gmail.com"
 LIC="gpl3"
+HOMEPAGE="https://mystiq.swl-x.info"
+VCS="https://github.com/llamaret/mystiq.git"
+DEPS="libc6, ffmpeg (>=3.0.0), qt5-default (>=5.9.0), libqt5multimedia5-plugins (>=5.9.0)"
+RECOM="sox, libnotify-bin, mplayer"
+DATE=`date +"%a, %d %b 20%y %H:%M:%S %z"`
+
+# var listing for dynamic substitution
+VARS="VERSION NAME MAINT EMAIL HOMEPAGE VCS DEPS RECOM DATE"
 
 mkdir "$SRCDIR"
 cp -r "$PWD"/* "$SRCDIR"/
@@ -23,15 +31,29 @@ qmake .
 make clean
 rm -rdf AppDir
 rm -rdf packaging-Rosa-rpm
-rm *.deb
+rm -f *.deb
 
 # make it happen
 dh_make  -c $LIC -e $EMAIL -s -p $NAME -y --createorig
+
+# provision the debian folder
+for v in `echo $VARS | xargs` ; do
+    # get the var content
+    CONTp=${!v}
+    
+    #escape possible / in the files
+    CONT=`echo ${CONTp//\//\\\\/}`
+
+    # note
+    echo "replace $v by \"$CONT\""
+
+    find "debian/" -type f -exec \
+        sed -i s/"\_$v\_"/"${CONT}"/g {} \;
+done
 
 # make the debian archive
 debuild -us -uc
 
 cd ..
 cp *.deb "$HOME"/
-
 rm -rdf "$TMPDIR"
